30e977163de72f53c6f3b07fb0a4397afae03b8b
최영철 <veldise@gmail.com>
2021-10-13 10:38:30 +0900

    feat: oauth2-server@2.x 기준으로 서버 구축

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+/**
+ *
+ */
+const path = require('path');
+const express = require('express');
+const oauthserver = require('oauth2-server');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const createError = require('http-errors');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+var memorystore = require('./model');
+
+app.oauth = oauthserver({
+  model: memorystore,
+  grants: ['password','refresh_token'],
+  debug: true
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +30,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/dump', function (req, res) {
+  res.send(memorystore.dumpJSON());
+});
+app.all('/oauth/token', app.oauth.grant());
+app.get('/test', function (req, res) {
+  res.send('No secret area: test');
+});
+app.get('/test2', app.oauth.authorise(), function (req, res) {
+  res.send('Secret area: test2');
+});
+app.use(app.oauth.errorHandler());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
